520bf94223e0b2e377f0bcda8a44d2729d3508a4
gprose1234gmail <95291555+gprose1234gmail@users.noreply.github.com>
2021-12-01 17:42:46 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -26,7 +26,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 
 PM_START_TEXT = """
-**Hello {}, My Name is {}!** 
+**Hello {}, My Name is AJAY ** 
 I am an **SUPERB**  group management bot.
 You can find the list of available commands with /help.
 
@@ -49,7 +49,7 @@ Hello! my name *{}*.
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-TECHNO_IMG = "https://telegra.ph/file/84b2017bc2f3c90f2e61c.jpg"
+TECHNO_IMG = "https://telegra.ph/file/815f25824ad35645c6a3d.jpg"
 IMPORTED = {}
 MIGRATEABLE = []
 HELPABLE = {}
@@ -142,11 +142,18 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/teamishere")],
-                                                                                   [InlineKeyboardButton(text="ADD GRAND OFFICIAL TO YOUR GROUP",
+                                                                       url="t.me/ajaymashaaa")],
+                                                                                   [InlineKeyboardButton(text="ADD ONLY AJAY TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
+                                                                                    InlineKeyboardButton(text="public chating group", url="https://t.me/Tamil_Friendship_Group"),[
+
+        InlineKeyboardButton(text="songs", url="https://t.me/YUVANPLAYLIST"),
+
+        InlineKeyboardButton(text="public chatting group ", url="https://t.me/Tamil_Friendship_Group"),],
+
+
                                                                                    InlineKeyboardButton(text="Source Code",
-                                                                       url="https://github.com/legendx22/GRANDROBOT")
+                                                                       url="https://github.com/gprose1234gmail/GRANDROBOT")
                                                                                  ]]))
 
     else:
